5386b187bab3403ea6cc20f380a50d0bbd08a44e
Nguyenhanam785 <nguyenhanam785@gmail.com>
2025-03-07 10:24:32 +0700

    btvn ss22

--- ss22/b8.js
+++ ss22/b8.js
@@ -1,19 +1,19 @@
-let number = prompt("Nhập một chuỗi:");
-
-if (number && !isNaN(Number(number)) && Number(number) >= 0) { 
-    let arr = number.split("").map(Number);
-    let frequency = {};
-    let maxCount = 0;
-    let mostFrequent = null;
-    for (let num of arr) {
-        frequency[num] = (frequency[num] || 0) + 1;
-        if (frequency[num] > maxCount) {
-            maxCount = frequency[num];
-            mostFrequent = num;
-        }
-    }
-
-    console.log("Số xuất hiện nhiều nhất là: " + mostFrequent);
-} else {
-    console.log("Không hợp lệ");
+let numbers = [2, 5, 7, 4, 1, 8, 6, 2, 5, 7];
+let frequency = {};
+for (let num of numbers) {
+    frequency[num] = (frequency[num] || 0) + 1;
 }
+let maxCount = 0;
+let mostFrequent = null;
+
+for (let num in frequency) {
+    if (
+        frequency[num] > maxCount || 
+        (frequency[num] === maxCount && num < mostFrequent)
+    ) {
+        maxCount = frequency[num];
+        mostFrequent = Number(num);
+    }
+}
+
+console.log("Phần tử xuất hiện nhiều nhất là: " + mostFrequent);
